8f87bdfd93ea113824af561e1528bc682510d88a
lee <leesjensen@gmail.com>
2023-04-28 14:25:34 -0600

    add() md rendering

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
+//import { setupCounter } from './counter.js'
+
+import MarkdownIt from 'markdown-it'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github.css';
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+const md = MarkdownIt({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+               `</code></pre>`;
+      } catch (__) {}
+    }
+
+    return '';
+  }
+});
+
+
+const rootURL = 'https://raw.githubusercontent.com/webprogramming260/.github/main/profile/css/debuggingCss/'
+
+
+const r = await fetch(rootURL + 'debuggingCss.md')
+const body = await r.text()
+
+const reg = /\!\[(.*)\]\((.*)\)/g
+
+ const up = body.replaceAll(reg, '![$1]('+rootURL+'$2)')
+
+let h = md.render(up)
+
+h = h + `<a href="https://github.com/webprogramming260/.github/blob/main/profile/css/debuggingCss/debuggingCss.md">GitHub</a>`
+
+
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div id="md" class="card">
+      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector("#md").innerHTML = h;
+
+
+// setupCounter(document.querySelector('#counter'))
