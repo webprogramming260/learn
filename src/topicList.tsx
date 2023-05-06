76455ed034307a0c3ba25470335d66fcf2996cec
lee <leesjensen@gmail.com>
2023-05-06 11:15:38 -0600

    style (topics) simple animation

--- src/topicList.tsx
+++ src/topicList.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './topicList.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./topicList.css";
 
 export default function TopicList({ topics }) {
   function getDue(dueDate: Date) {
     if (dueDate) {
       const now = new Date();
       const due = now.getTime() < dueDate.getTime();
-      return <span className={due ? 'due' : 'past-due'}>({`${dueDate.getMonth() + 1}/${dueDate.getDate()}`})</span>;
+      return (
+        <span className={due ? "due" : "past-due"}>
+          ({`${dueDate.getMonth() + 1}/${dueDate.getDate()}`})
+        </span>
+      );
     }
-    return '';
+    return "";
   }
   const o: JSX.Element[] = [];
   topics.forEach((section) => {
@@ -17,15 +21,26 @@ export default function TopicList({ topics }) {
     section.topics.forEach((topic) => {
       ol.push(
         <li key={topic.title}>
-          <NavLink className="px-4" to={`/page/${topic.path}`}>{topic.title}</NavLink> {getDue(topic.due)}
+          <NavLink
+            className="px-4 hover:animate-pulse "
+            to={`/page/${topic.path}`}
+          >
+            {topic.title}
+          </NavLink>{" "}
+          {getDue(topic.due)}
         </li>
       );
     });
 
     if (ol.length > 0) {
       o.push(
-        <div key={section.title} className="container m-5 p-4 bg-stone-200 rounded-xl dark:bg-stone-800 mx-auto">
-          <h2 className="font-bold text-2xl p-3 text-stone-900 dark:text-teal-100">{section.title}</h2>
+        <div
+          key={section.title}
+          className="container m-5 p-4 bg-stone-200 rounded-xl dark:bg-stone-800 mx-auto"
+        >
+          <h2 className="font-bold text-2xl p-3 text-stone-900 dark:text-teal-100">
+            {section.title}
+          </h2>
           <ul>{ol}</ul>
         </div>
       );
