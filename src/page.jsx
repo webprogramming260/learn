f683d3d801da45950386774fcbbb6155db5a1900
Lee <leesjensen@gmail.com>
2023-05-06 22:12:24 -0600

    fix(page) code rendering colors to be visible in dark mode

--- src/page.jsx
+++ src/page.jsx
@@ -1,27 +1,23 @@
-import React from "react";
-import { NavLink, useParams, useLocation } from "react-router-dom";
+import React from 'react';
+import {NavLink, useParams, useLocation} from 'react-router-dom';
 
-import MarkdownIt from "markdown-it";
-import hljs from "highlight.js"; // Formats the code blocks
-import "highlight.js/styles/github.css";
-import "./page.css";
-import "./github-markdown.css";
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js'; // Formats the code blocks
+import 'highlight.js/styles/github.css';
+import './page.css';
+import './github-markdown.css';
 
-export default function Page({ onNav }) {
+export default function Page({onNav}) {
   const location = useLocation();
-  const wildcard = useParams()["*"];
-  const url =
-    "https://github.com/webprogramming260/.github/blob/main/profile/" +
-    wildcard;
-  const gitHubUrl = url.replace("_", ".");
-  const [htmlDoc, setHtmlDoc] = React.useState(
-    "<div style='height:120vh;'></div>"
-  );
+  const wildcard = useParams()['*'];
+  const url = 'https://github.com/webprogramming260/.github/blob/main/profile/' + wildcard;
+  const gitHubUrl = url.replace('_', '.');
+  const [htmlDoc, setHtmlDoc] = React.useState("<div style='height:120vh;'></div>");
 
   React.useEffect(() => {
     let rawUrl = gitHubUrl.replace(
-      "github.com/webprogramming260/.github/blob",
-      "raw.githubusercontent.com/webprogramming260/.github"
+      'github.com/webprogramming260/.github/blob',
+      'raw.githubusercontent.com/webprogramming260/.github'
     );
     const [, rootUrl] = /(.*\/)([^\/]*)$/.exec(rawUrl);
 
@@ -33,7 +29,7 @@ export default function Page({ onNav }) {
 
         let renderedHtml = md.render(up);
         renderedHtml = renderedHtml.replaceAll(
-          "Canvas",
+          'Canvas',
           '<a href="https://byu.instructure.com/courses/21349/assignments">Canvas</a>'
         );
 
@@ -44,25 +40,21 @@ export default function Page({ onNav }) {
   return (
     <>
       <PageNav onNav={onNav} url={url} gitHubUrl={gitHubUrl}></PageNav>
-      <div className="flex align-middle justify-center">
-        <div
-          id="md"
-          className="markdown-body"
-          dangerouslySetInnerHTML={{ __html: htmlDoc }}
-        ></div>
+      <div className='flex align-middle justify-center'>
+        <div id='md' className='markdown-body' dangerouslySetInnerHTML={{__html: htmlDoc}}></div>
       </div>
       <PageNav onNav={onNav} url={url} gitHubUrl={gitHubUrl}></PageNav>
     </>
   );
 }
 
-function PageNav({ onNav, url, gitHubUrl }) {
+function PageNav({onNav, url, gitHubUrl}) {
   return (
-    <div className="m-0 text-gray-200 bg-gray-800 justify-between flex px-6 py-3 text-lg">
-      <NavLink to={onNav("prev", url)}>Prev</NavLink>
-      <NavLink to="/">Topics</NavLink>
+    <div className='m-0 text-gray-200 bg-gray-800 justify-between flex px-6 py-3 text-lg'>
+      <NavLink to={onNav('prev', url)}>Prev</NavLink>
+      <NavLink to='/'>Topics</NavLink>
       <a href={gitHubUrl}>GitHub</a>
-      <NavLink to={onNav("next", url)}>Next</NavLink>
+      <NavLink to={onNav('next', url)}>Next</NavLink>
     </div>
   );
 }
@@ -71,14 +63,10 @@ const md = MarkdownIt({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return (
-          "<pre>" +
-          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
-          `</pre>`
-        );
+        return '<pre>' + hljs.highlight(str, {language: lang, ignoreIllegals: true}).value + `</pre>`;
       } catch (__) {}
     }
 
-    return "";
+    return '';
   },
 });
