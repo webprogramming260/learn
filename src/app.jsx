0de79f9812a6c4094dc25c6f53859e1455fb062c
Lee <leesjensen@gmail.com>
2023-05-10 10:27:44 -0600

    enhance(topics) refactored to multiple components

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import TopicList from "./topicList";
-import Page from "./page";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./scrollToTop";
+import React from 'react';
+import TopicList from './topicList';
+import Page from './page';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import ScrollToTop from './scrollToTop';
 
 export default function App() {
   function navPage(direction, topicUrl) {
@@ -15,12 +15,12 @@ export default function App() {
         }
 
         if (topicUrl.endsWith(topic.path)) {
-          if (direction === "next") {
+          if (direction === 'next') {
             next = true;
           } else if (prev) {
             return `/page/${prev.path}`;
           }
-        } else if (direction === "prev") {
+        } else if (direction === 'prev') {
           prev = topic;
         }
       }
@@ -40,29 +40,29 @@ export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <div className="text-stone-950 dark:text-stone-300 dark:bg-stone-900 flex flex-col">
-        <h1 className="font-bold text-2xl p-4 text-stone-800 dark:text-stone-300 border-b-4 border-stone-900 dark:border-stone-400 flex flex-row align-middle justify-start">
+      <div className='text-stone-950 dark:text-stone-300 dark:bg-stone-900 flex flex-col'>
+        <h1 className='font-bold text-2xl p-4 text-stone-800 dark:text-stone-300 border-b-4 border-stone-900 dark:border-stone-400 flex flex-row align-middle justify-start'>
           <svg
-            className="fill-yellow-200 dark:fill-cyan-900 h-8 mx-3 inline"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1.5"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
+            className='fill-yellow-200 dark:fill-cyan-900 h-8 mx-3 inline'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='1.5'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+            aria-hidden='true'
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z'
             ></path>
           </svg>
           CS 260 - Web Programming
         </h1>
         <Routes>
-          <Route path="/" element={<TopicList topics={topics} />} exact />
-          <Route path="/page/*" element={<Page onNav={navPage} />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path='/' element={<TopicList topics={topics} />} exact />
+          <Route path='/page/*' element={<Page onNav={navPage} />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
@@ -74,12 +74,11 @@ function NotFound() {
 }
 
 async function loadTopics() {
-  const url =
-    "https://github.com/webprogramming260/.github/blob/main/profile/instructionTopics.md";
+  const url = 'https://github.com/webprogramming260/.github/blob/main/profile/instructionTopics.md';
 
   const rawUrl = url.replace(
-    "github.com/webprogramming260/.github/blob",
-    "raw.githubusercontent.com/webprogramming260/.github"
+    'github.com/webprogramming260/.github/blob',
+    'raw.githubusercontent.com/webprogramming260/.github'
   );
 
   const r = await fetch(rawUrl);
@@ -89,9 +88,9 @@ async function loadTopics() {
   const sections = [];
   for (let blockMatch of body.matchAll(blockRegEx)) {
     const topics = [];
-    sections.push({ title: blockMatch[1], topics: topics });
+    sections.push({title: blockMatch[1], topics: topics});
     for (let lineMatch of blockMatch[0].matchAll(lineRegEx)) {
-      const path = lineMatch[3].replaceAll(".", "_");
+      const path = lineMatch[3].replaceAll('.', '_');
       topics.push({
         assignment: !!lineMatch[1],
         title: lineMatch[2],
@@ -105,7 +104,7 @@ async function loadTopics() {
 
 function parseDate(textDate) {
   if (textDate) {
-    const parts = textDate.split("/");
+    const parts = textDate.split('/');
     if (parts.length === 2) {
       let date = new Date();
       date.setMonth(parts[0] - 1);
--- src/topicList.tsx
+++ src/topicList.tsx
@@ -1,51 +1,64 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./topicList.css";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './topicList.css';
 
-export default function TopicList({ topics }) {
+export default function TopicList({topics}) {
+  const topicSections: Element[] = [];
+  topics.forEach((section) => {
+    const topicSection = <TopicSection section={section} />;
+    topicSections.push(topicSection);
+  });
+
+  return <div>{topicSections}</div>;
+}
+
+function TopicSection({section}) {
+  const [showTopics, setShowTopics] = React.useState(false);
+
+  const ol: Element[] = [];
+
+  section.topics.forEach((topic) => {
+    ol.push(<Topic topic={topic} />);
+  });
+
+  if (ol.length > 0) {
+    const animate = showTopics ? '' : 'hover:animate-wiggle';
+    return (
+      <div key={section.title} className='container m-5 p-4 bg-stone-200 rounded-xl dark:bg-stone-800 mx-auto'>
+        <h2
+          className={animate + ' cursor-pointer font-bold text-2xl p-3 text-stone-900 dark:text-teal-100'}
+          onClick={() => setShowTopics(!showTopics)}
+        >
+          {section.title}
+        </h2>
+        {showTopics && (
+          <div className='block'>
+            <ul>{ol}</ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return <div></div>;
+}
+
+function Topic({topic}) {
   function getDue(dueDate: Date) {
     if (dueDate) {
       const now = new Date();
       const due = now.getTime() < dueDate.getTime();
-      return (
-        <span className={due ? "due" : "past-due"}>
-          ({`${dueDate.getMonth() + 1}/${dueDate.getDate()}`})
-        </span>
-      );
+      return <span className={due ? 'due' : 'past-due'}>({`${dueDate.getMonth() + 1}/${dueDate.getDate()}`})</span>;
     }
-    return "";
+    return '';
   }
-  const o: JSX.Element[] = [];
-  topics.forEach((section) => {
-    const ol: JSX.Element[] = [];
-    section.topics.forEach((topic) => {
-      ol.push(
-        <li key={topic.title}>
-          <NavLink
-            className="px-4 hover:animate-pulse "
-            to={`/page/${topic.path}`}
-          >
-            {topic.title}
-          </NavLink>{" "}
-          {getDue(topic.due)}
-        </li>
-      );
-    });
 
-    if (ol.length > 0) {
-      o.push(
-        <div
-          key={section.title}
-          className="container m-5 p-4 bg-stone-200 rounded-xl dark:bg-stone-800 mx-auto"
-        >
-          <h2 className="font-bold text-2xl p-3 text-stone-900 dark:text-teal-100">
-            {section.title}
-          </h2>
-          <ul>{ol}</ul>
-        </div>
-      );
-    }
-  });
-
-  return <div>{o}</div>;
+  return (
+    <li key={topic.title}>
+      <NavLink className='px-4 hover:animate-pulse ' to={`/page/${topic.path}`}>
+        {topic.title}
+      </NavLink>{' '}
+      {getDue(topic.due)}
+    </li>
+  );
 }
