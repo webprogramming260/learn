fe986b0d15d882e49a36286568e1aaf949b3bf72
Lee <lee.jensen@agilix.com>
2024-08-14 08:28:50 -0600

    fix(deploy) to work for 260 again

--- deploy.sh
+++ deploy.sh
@@ -24,13 +24,18 @@ npm run build
 # Step 2
 printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
-rm -rf /app/learn/public_html
-mkdir -p /app/learn/public_html
+rm -rf public_html/instruction
+mkdir -p public_html/instruction
+# These are for 240. We need to unify all of this
+# rm -rf /app/learn/public_html
+# mkdir -p /app/learn/public_html
 ENDSSH
 
 # Step 3
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" dist/* ubuntu@$hostname:/app/learn/public_html
+scp -r -i "$key" dist/* ubuntu@$hostname:public_html/instruction
+# These are for 240. We need to unify all of this
+# scp -r -i "$key" dist/* ubuntu@$hostname:/app/learn/public_html
 
 # Step 4
 rm -rf dist
